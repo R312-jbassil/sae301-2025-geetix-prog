0efea37bd190f920c1a2c6fb1d6284471ce89466
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2025-11-05 21:17:01 +0100

    local

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -2,7 +2,7 @@ import PocketBase from "pocketbase";
 import type { TypedPocketBase } from "./pocketbase-types";
 var path = "";
 if (import.meta.env.MODE === "development")
-  path = "http://localhost:8090"; //localhost = machine de dev
-else path = "https://sae301.mathis-guellati.fr:443"; //url du site
+  path = "http://localhost:8085"; //localhost = machine de dev
+else path = "http://localhost:8085"; //url du site
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
