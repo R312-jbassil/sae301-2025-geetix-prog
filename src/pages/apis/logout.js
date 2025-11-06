ac6e81a939ee291d1e585b757e1ce539cb889cfd
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2025-11-06 10:47:34 +0100

    lougout encore

--- src/pages/apis/logout.js
+++ src/pages/apis/logout.js
@@ -1,9 +1,12 @@
-export const POST = async ({ cookies, redirect }) => {
+export const POST = async ({ cookies }) => {
   cookies.delete("pb_auth", {
     path: "/",
     maxAge: 0,
     expires: new Date(0),
+    httpOnly: false,
+    secure: true,
+    sameSite: "lax",
   });
 
-  return redirect("/connexion", 303);
+  return new Response(JSON.stringify({ success: true }), { status: 200 });
 };
