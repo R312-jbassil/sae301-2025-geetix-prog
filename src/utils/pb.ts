b5ac374e17b500da1f83207499bbafe5a5893207
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2025-11-06 11:32:20 +0100

    fix

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -2,7 +2,7 @@ import PocketBase from "pocketbase";
 
 // Use local PocketBase when developing, production URL when deployed
 const DEV_URL = "http://127.0.0.1:8090";
-const PROD_URL = "https://sae301.mathis-guellati.fr";
+const PROD_URL = "https://sae301.mathis-guellati.fr/_/";
 
 const baseURL = import.meta.env.MODE === "development" ? DEV_URL : PROD_URL;
 
